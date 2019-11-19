bcbfbe7d549db017ed261b18edeba01bdafdf2b8
unknown <unknown@example.com>
2019-11-19 14:35:29 +0800

    添加文件
    首页目录,readMe

---
+++ readMe.js
@@ -0,0 +1,15 @@
+/*注释
+"pages/home/home", 首页
+"pages/login/login", 登录
+"pages/mine/mine", 我的
+"pages/personalData/personalData", 个人信息
+"pages/vehicleFiles/vehicleFiles", 车辆档案列表
+"pages/vehicleDetailInfo/vehicleDetailInfo", 车辆信息详情
+"pages/vehicleRepairRecord/vehicleRepairRecord", 车辆维修记录详情
+"pages/verifyEnterpriseList/verifyEnterpriseList", (现场勘验)勘验企业列表
+"pages/verifyingEnterprise/verifyingEnterprise",(现场勘验)勘验企业详情
+"pages/enterpriseFileRecordList/enterpriseFileRecordList", 备案企业列表
+"pages/enterpriseFileRecordDetail/enterpriseFileRecordDetail",备案企业详情
+"pages/fileRecordAuditList/fileRecordAuditList", 备案审核列表
+"pages/fileRecordAuditDetail/fileRecordAuditDetail",备案审核详情
+*/
